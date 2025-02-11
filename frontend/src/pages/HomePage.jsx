7d271ada2f8e4d17cce64b0187575b56d3dd5f72
madgoat42069 <mad42069@proton.me>
2025-02-11 22:02:42 +0200

    made a button on the homepage if its empty to redirect to createProduct

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,43 @@
+import { Container } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
     return (
-        <div>
-            HomePage
-        </div>
+        <Container maxW="container.xl" py={12}>
+            <VStack spacing={8}>
+                <Text
+                    fontSize={"30"}
+                    fontWeight={"bold"}
+                    bgGradient={"linear(to-r, magenta.400, purple.400)"}
+                    bgClip={"text"}
+                    textAlign={"center"}
+                >
+                    Current Products
+                </Text>
+
+                <Text
+                    fontSize="xl"
+                    textAlign={"center"}
+                    fontWeight={"bold"}
+                    color={"purple.400"}
+                >
+                    No products found {" "}
+                    <Link to={"/create"}>
+                        <Text
+                            as="span"
+                            color={"purple.700"}
+                            _hover={{
+                                textDecoration: "underline"
+                            }}
+                        >
+                            Create a product
+                        </Text>
+                    </Link>
+                </Text>
+
+            </VStack>
+        </Container>
 )}
 
 export default HomePage;
