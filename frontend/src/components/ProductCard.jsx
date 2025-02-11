c9b2f808d42ebef3c6231ea5a961374323dab81a
madgoat42069 <mad42069@proton.me>
2025-02-11 22:26:18 +0200

    created a productCard component

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,28 @@
+import { Box, Image } from '@chakra-ui/react';
+
+const ProductCard = ({product}) => {
+    return (
+        <Box
+            shadow="lg"
+            rounded="lg"
+            overflow="hidden"
+            transition="all 0.25s"
+            _hover={{
+                transform: "translateY(-5px)", 
+                shadow: "xl"
+            }}
+        >
+            <Image
+                src={product.image}
+                alt={product.name}
+                h={48}
+                w="full" objectFit="cover"/>
+
+            <Box p={4}>
+
+            </Box>
+        </Box>
+    )}
+
+export default ProductCard;
+
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,18 @@
 import { Container } from "@chakra-ui/react";
 import { Text, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { SimpleGrid } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useProductStore } from "../store/product";
 
 const HomePage = () => {
+    
+    const {fetchProducts, products} = useProductStore();
+    useEffect(() => {
+        fetchProducts();
+    }, [fetchProducts]);
+    console.log("products", products);
+
     return (
         <Container maxW="container.xl" py={12}>
             <VStack spacing={8}>
@@ -15,6 +25,17 @@ const HomePage = () => {
                 >
                     Current Products
                 </Text>
+
+                <SimpleGrid
+                    columns={{ base: 1, md: 2, lg: 3}}
+                    spacing={10}
+                    w={"full"}
+                    
+                >
+                    {products.map((product) => (
+                        <ProductCard key={products._id} product={product}/>
+                    ))}
+                </SimpleGrid>
 
                 <Text
                     fontSize="xl"
